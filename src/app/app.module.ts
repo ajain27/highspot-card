46c3f7717601c79c954ca661df921d7447cf79a4
Ankit Jain <ankit.jain67@t-mobile.com>
2020-11-06 14:52:09 -0800

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { CardsComponent } from './cards/cards.component';
+import { SearchComponent } from './search/search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardsComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from "@angular/material/card";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatFormFieldModule,  MatInputModule } from '@angular/material';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule
+]
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports:[MaterialComponents]
+})
+export class MaterialModule { }
